42f4229099f3a8e30f0e8d939d2cba8411156038
qinhaihang <hai449769034@qq.com>
2019-07-22 23:02:03 +0800

    Initial commit

---
+++ app/src/main/java/wxplus/opengles2forandroid/MainActivity.java
@@ -0,0 +1,34 @@
+package wxplus.opengles2forandroid;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle(R.string.app_name);
+        setContentView(R.layout.activity_main);
+        // test
+        //click_height_map(null);
+    }
+
+
+    // onclick start
+    public void click_simple_color(View view) {
+        startActivity(OpenGL_01_Simple_Color.class);
+    }
+    public void click_simple_texture(View view) {
+        startActivity(OpenGL_02_Simple_Texture.class);
+    }
+    public void click_simple_object(View view) {
+        startActivity(OpenGL_03_Simple_Object.class);
+    }
+    public void click_sky_box(View view) {
+        startActivity(OpenGL_04_Skybox.class);
+    }
+    public void click_height_map(View view) {
+        startActivity(OpenGL_05_HeightMap.class);
+    }
+}
